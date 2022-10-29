a674a5aace0459827cde224cd8fae2f411dfa775
My lab pc <labsamplepc@gmail.com>
2022-10-29 14:52:56 +0530

    router setted,created variables using usestate to store data

--- src/Pages/Mainpage.jsx
+++ src/Pages/Mainpage.jsx
@@ -7,7 +7,7 @@ function Mainpage(props) {
   return (
     <div className='flex flex-col bg-[#F9F9F9] items-center scroll-m-0'>
         <div>
-          <h1 className='text-center mt-10  font-bold text-[20px] md:text-[30px] '>Hey, USER</h1>
+          <h1 className='text-center mt-10  font-bold text-[20px] md:text-[30px] '>Hey, USERNAME</h1>
           <h1 className='text-center m-5 font-bold text-[20px] md:text-[30px] '>Hey, What's the plan for today.</h1>
           <div className='justify-between shadow-md border-[0.5px]  w-screen max-w-[400px] md:max-w-[500px] border-gray-200 rounded-md  flex h-16 px-4 items-center bg-gradient-to-r from-[#00FFAB] to-[#00ffaaf4] '>
             <input type="text" className='h-[50px] w-[70%] rounded-md outline-none p-2 md:w-100%' onChange={(e)=>{setTodo(e.target.value)}}/>
