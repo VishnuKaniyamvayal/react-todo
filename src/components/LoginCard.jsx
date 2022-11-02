27c65dabcfc67d14d54818506c17f5f333448e4d
My lab pc <labsamplepc@gmail.com>
2022-11-02 11:31:25 +0530

    some errors yet to solve

--- src/components/LoginCard.jsx
+++ src/components/LoginCard.jsx
@@ -10,20 +10,23 @@ function LoginCard() {
     const [email, setEmail] = useState()
     const [password,setPassword] = useState()
     
-    const handlesubmit = (e)=>{
-      e.preventdefault()
-      const auth = getAuth()
-      signInWithEmailAndPassword(auth,email,password).then(()=>{navigate("/home")})
-    }
+    const handlelogin =(e)=>{ 
+      setLoading(true)
+      e.preventDefault()
+      const auth=getAuth();
+      signInWithEmailAndPassword(auth,email,password)
+      navigate('/home')
+      
+      }
 
     return (
     <div >
         <div className={`flex flex-col px-10 border-[2px] m-4 rounded-lg shadow-xl max-w-[465px] mx-auto z-100 `}>
             <CgUserlane className={`mx-auto mt-6 ${loading?'animate-pulse':""} `} size={90}/>
             <input type="text" className={`border rounded-md mb-6 mt-12 h-12 pl-4 border-gray-400 `} placeholder='Enter your Email' onChange={(e)=>{setEmail(e.target.value)}}/>
-            <input  type="text" className='border rounded-md h-12 mb-4 pl-4 border-gray-400' placeholder='Enter your password' onChange={(e)=>{setPassword(e.target.value)}}/>
+            <input  type="password" className='border rounded-md h-12 mb-4 pl-4 border-gray-400' placeholder='Enter your password' onChange={(e)=>{setPassword(e.target.value)}}/>
             <div className='flex flex-row'>
-                <button className='mb-4 bg-[#00FFAB] w-20 h-10 justify-center mx-auto rounded-md hover:bg-[#0df2a6] duration-300 focus:bg-[#14C38E] font-medium z-10' onClick={()=>{setLoading(true).then(handlesubmit())}}>Login</button>
+                <button className='mb-4 bg-[#00FFAB] w-20 h-10 justify-center mx-auto rounded-md hover:bg-[#0df2a6] duration-300 focus:bg-[#14C38E] font-medium z-10' onClick={handlelogin}>Login</button>
             </div>
             <p className='mx-auto m-2 mb-4'color>New user? <a className='text-[#14C38E] cursor-pointer'>  Sign up</a></p>
         </div>
